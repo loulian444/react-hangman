85c27ae26a0daabccc32d89c0517024699fc6309
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-08-02 16:40:43 -0400

    needed to add require

--- src/components/GuessForm.jsx
+++ src/components/GuessForm.jsx
@@ -1,19 +1,22 @@
 const GuessForm = ({ guessCollection, setGuessedLetter }) => {
-
   const submitHandler = (event) => {
     event.preventDefault();
     setGuessedLetter(event.target.children[0].value);
-    event.target.reset()
+    console.log(event.target.children);
+    event.target.reset();
   };
 
   return (
     <>
       <h2>Guess Form</h2>
-      <p>Your Guesses: {guessCollection.map((guess) => {
-        return `${guess} `
-      })}</p>
+      <p>
+        Your Guesses:{" "}
+        {guessCollection.map((guess) => {
+          return `${guess} `;
+        })}
+      </p>
       <form onSubmit={submitHandler}>
-        <input type="text" size={3} pattern="[a-z]" maxLength={1} />
+        <input type="text" size={3} pattern="[a-z]" maxLength={1} required />
         <button>Submit</button>
       </form>
     </>
